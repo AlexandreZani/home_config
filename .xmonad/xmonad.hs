de99326f27203832c955725c6d5e054b6cbc4529
Alex Zani <azani@google.com>
2012-02-28 12:20:31 -0800

    Added all the safe stuff.

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,8 @@
+import XMonad
+import XMonad.Util.EZConfig
+
+main = xmonad $ defaultConfig
+    { terminal = "terminator" }
+    `additionalKeysP`
+    [ ("C-M-l", spawn "xlock -model ant")
+    , ("M-S-p", spawn "exe=`dmenu_path | dmenu` && eval \"exec sudo -A $exe\"") ]
