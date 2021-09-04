678052b9a30cfa64a935fb4a393c2b5f41c935c2
Alex Zani <alex@zfc.io>
2021-09-04 00:49:11 -0700

    Switch most emacs module to be downloaded from MELPA.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,6 +3,36 @@
      (when (file-exists-p local-emacs-config)
        (load local-emacs-config)))
 
+;; Add the melpa packages
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(setq package-selected-packages '(lean-mode fstar-mode))
+
+(let ((need-to-install nil))
+  (dolist (p package-selected-packages)
+    (unless (require p nil t)
+      (setq need-to-install t)
+      (print (concat "need to install: " (symbol-name p)))
+      )
+    )
+  (when need-to-install
+    (package-refresh-contents)
+    (package-install-selected-packages)))
+
+(package-autoremove)
+
+(show-paren-mode 1)
+
+;; Add the submodule packages
+(setq submodule-packages '(google-c-style clang-format))
+
+(dolist (p submodule-packages)
+  (add-to-list 'load-path (concat "~/.emacs.d/" (symbol-name p))))
+(dolist (p submodule-packages)
+  (require p))
+
 ;; display line numbers
 (global-display-line-numbers-mode)
 
@@ -18,21 +48,6 @@
 
 ;; stop showing the welcome buffer
 (setq inhibit-startup-screen t)
-
-;; Add the submodules
-(setq submodule-packages '(google-c-style
-			   clang-format
-			   ;; lean-mode dependencies
-			   dash f flycheck s
-			   lean-mode
-			   fstar-mode
-			   ))
-
-(dolist (p submodule-packages)
-  (add-to-list 'load-path (concat "~/.emacs.d/" (symbol-name p))))
-(dolist (p submodule-packages)
-  (require p))
-
 
 ;; format c++ files on save.
 (defun c++-mode-before-save-hook ()
