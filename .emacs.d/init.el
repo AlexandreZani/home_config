fa2084e9394e9c69c79eb2ca7b1dad446f3fbcc8
Alex Zani <alex@zfc.io>
2021-10-23 22:57:35 -0700

    [emacs] Update the background color for fstar.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -61,3 +61,16 @@
 
 (custom-set-variables
  '(markdown-command "/usr/bin/pandoc"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fstar-subp-overlay-processed-face ((((background light)) (:background "#EAF8FF" :extend t)) (((background dark)) (:box nil)))))
