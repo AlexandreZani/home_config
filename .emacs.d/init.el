97d1c966742b6a9b066406a281147e0c57294348
Alex Zani <alex@zfc.io>
2022-04-23 05:40:32 -0700

    Configure emacs and bashrc for agda.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -78,3 +78,10 @@
 
 (load-file (let ((coding-system-for-read 'utf-8))
                 (shell-command-to-string "agda-mode locate")))
+
+;; auto-load agda-mode for .agda and .lagda.md
+(setq auto-mode-alist
+   (append
+     '(("\\.agda\\'" . agda2-mode)
+       ("\\.lagda.md\\'" . agda2-mode))
+     auto-mode-alist))
